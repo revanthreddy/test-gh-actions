0ff530718e725b9d71223349e89c3434044ff765
Revanth Anireddy <revanth.reddy@gmail.com>
2023-08-12 21:50:51 -0500

    file update

--- scripts/publish-artifacts.sh
+++ scripts/publish-artifacts.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "Total arguments : $#"
+# Get the arguments from the command line
+args=$@
+
+# Loop over the arguments
+for arg in $args; do
+  # Print the argument
+  echo $arg
+done
