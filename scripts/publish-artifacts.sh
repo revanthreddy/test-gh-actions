0f0f544fdf8d2264fd7ae3445ed8aa15f524ad45
Revanth Anireddy <revanth.reddy@gmail.com>
2023-08-13 16:26:04 -0500

    file update

--- scripts/publish-artifacts.sh
+++ scripts/publish-artifacts.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+
+
 echo "Total number of modules : $#"
 
 version_file_name="version.json"
@@ -12,23 +15,32 @@ for module_path in $module_paths; do
   # Check if the file exists in the directory
 
   if [ -f "$module_path/$version_file_name" ]; then
-    echo "FOUND FILE"
     # Get the key to extract
     key="version"
-    echo "$DAY_OF_WEEK"
 
-    # Open the JSON file
+    # Open the JSON file & Get the version_number for the key
     module_version_file=$(cat "$module_path/$version_file_name")
-    # Get the value for the key
-    value=$(echo "$module_version_file" | jq ".$key")
+    version_number=$(echo "$module_version_file" | jq ".$key")
 
-    if [ -z "$value" ]; then
+    if [ -z "$version_number" ]; then
       echo "The key $key does not exist in the JSON file $version_file_name."
       continue
     fi
+    # ZIP THE CONTENTS WITH VERSION NAME BEING THE ZIP FILE NAME
+
+    temp_directory=$(mktemp -d) # Create a temporary directory and copy files (excluding version.json)
+    for file in $(find "$module_path" -type f | grep -vE "$version_file_name"); do
+      cp "$file" "$temp_directory"
+    done
+
+    # Zip the contents of the temporary directory
+    zip -r "$version_number.zip" "$temp_directory"
+
+    # Remove the temporary directory
+    rm -rf "$temp_directory"
     # PUBLISH TO JFROG CODE GOES HERE
-    # Print the value
-    echo "Version : $value"
+    echo "Version : $version_number"
+
 
   else
     # The file does not exist
