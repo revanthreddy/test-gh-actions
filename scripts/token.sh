e1a260fac626f846815c5fe63e3caddb43c031e6
Revanth Anireddy <revanth.reddy@gmail.com>
2023-08-14 19:46:04 -0500

    update

--- scripts/token.sh
+++ scripts/token.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
-# Get the string to tokenize
-string="providers/aws/modules/s3"
+function tokenize_string() {
+  # Get the string to tokenize
+  token_array=()
+  string=$1
 
-# Split the string on the "/" delimiter
-tokens=$(echo $string | tr '/' '\n')
+  # Get the delimiter
+  delimiter=$2
 
-# Print the tokens
-for token in $tokens; do
-  echo $token
-done
+  # Split the string on the delimiter
+  tokens=$(echo $string | tr "$delimiter" '\n')
+
+  for token in $tokens; do
+    token_array+=($token)
+  done
+
+  # Get the first token
+  first_token=${token_array[0]}
+
+  size="${#token_array[@]}"
+
+  # Get the last token
+  last_token=${token_array[$size-1]}
+
+  # Print the first and the last token
+  echo "The first token is: $first_token"
+  echo "The last token is: $last_token"
+#  provider_index=1
+#  module_name_index=-1
+##  echo "${tokens[provider_index]}"
+##  echo "${tokens[module_name_index]}"
+#
+#  last_token=${tokens[-1]}
+
+  # Return the last token
+#  echo "$tokens"
+
+}
+
+# Call the function
+tokenize_string "providers/aws/modules/s3" "/"
