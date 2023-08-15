55418b8d84341a63d5dec786e0fe1f0400670b11
Revanth Anireddy <revanth.reddy@gmail.com>
2023-08-14 19:48:59 -0500

    update

--- scripts/token.sh
+++ scripts/token.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+array=()
+
 function tokenize_string() {
   # Get the string to tokenize
-  token_array=()
   string=$1
 
   # Get the delimiter
@@ -11,31 +12,16 @@ function tokenize_string() {
   # Split the string on the delimiter
   tokens=$(echo $string | tr "$delimiter" '\n')
 
+  # Add the tokens to the array
   for token in $tokens; do
-    token_array+=($token)
+    array+=($token)
   done
 
-  # Get the first token
-  first_token=${token_array[0]}
+  # Get the size of the array
+  size=${#array[@]}
 
-  size="${#token_array[@]}"
-
-  # Get the last token
-  last_token=${token_array[$size-1]}
-
-  # Print the first and the last token
-  echo "The first token is: $first_token"
-  echo "The last token is: $last_token"
-#  provider_index=1
-#  module_name_index=-1
-##  echo "${tokens[provider_index]}"
-##  echo "${tokens[module_name_index]}"
-#
-#  last_token=${tokens[-1]}
-
-  # Return the last token
-#  echo "$tokens"
-
+  # Print the size of the array
+  echo "The size of the array is: $size"
 }
 
 # Call the function
