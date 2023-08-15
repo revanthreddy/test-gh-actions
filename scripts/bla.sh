9c34f27cdbd4a183d101babc0ff23fee6029e1c3
Revanth Anireddy <revanth.reddy@gmail.com>
2023-08-14 20:15:18 -0500

    update

--- scripts/bla.sh
+++ scripts/bla.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+get_last_element() {
+    local input_string="$1"
+    IFS=" " read -ra tokens <<< "$input_string"
+    local num_tokens=${#tokens[@]}
+    local last_element="${tokens[num_tokens-1]}"
+    echo "$last_element"
+}
+
 # Input string
 input_string="This is a sample input string for tokenization"
 
-# Tokenize the input string using space as delimiter
-IFS=" " read -ra tokens <<< "$input_string"
-
-# Get the number of tokens
-num_tokens=${#tokens[@]}
-
-# Get the last element (token) from the array
-last_element="${tokens[num_tokens-1]}"
-
-# Print the last element
+# Call the function and print the result
+last_element=$(get_last_element "$input_string")
 echo "Last element: $last_element"
